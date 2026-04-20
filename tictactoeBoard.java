e72e17ac5cb0e3ad8c41950f7aa3072474190035
Meghna Das <md8226@srmist.edu.in>
2026-04-20 22:46:37 +0530

    Initial commit - Tic Tac Toe Board

---
+++ tictactoeBoard.java
@@ -0,0 +1,25 @@
+public class tictactoeBoard {
+
+    public static void main(String[] args) {
+
+        // Create a 3x3 board
+        char[][] board = new char[3][3];
+
+        // Initialize the board with '-'
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
+        }
+
+        // Print the board
+        System.out.println("Tic-Tac-Toe Board:");
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println(); // Move to next row
+        }
+    }
+}
